aec09c1c655829a691a7e709e5a1c1c5a8d3cb3a
Dov Tuch <tuchdov@gmail.com>
2025-08-13 16:29:43 +0300

    Add the grid creatin and coloring

--- script.js
+++ script.js
@@ -1,21 +1,39 @@
-// select the container
+// select the container and slider
 
 const container = document.querySelector('.container');
-
+const slider = document.querySelector('#slider');
 // create a nested loop to fit the slider
-function createGrid() {
-    for (let i = 0; i < (slider.value * slider.value); i++) {
-        const grid = document.createElement('div');
-        let a = slider.value;
-        const boxSize = 250 / a; // creates the dynamic squeres
-        grid.classList.add('grid');
-        grid.style.width = boxSize + 'px';
-        grid.style.height = boxSize + 'px';
-        container.appendChild(grid);
+
+let row = document.createElement('div');
+let col = document.createElement('div');
+
+const createGrid = () => {
+    const input = slider.value;
+    for(i = 0; i < input*input; i++) {
+            let square = document.createElement('div');
+            square.classList.add('square');
+            container.appendChild(square);
+            const boxSize = 512 / input; // 'a' was the slider value
+            square.style.width = boxSize + 'px';
+            square.style.height = boxSize + 'px';
+            square.style.boxSizing = 'border-box';
+            square.style.border = 'solid black';
+            square.addEventListener('mouseover',()=>{
+                square.style.backgroundColor = 'DimGrey';
+            })
+
+    }
+ }
+
+const emptyGrid = () => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
     }
 }
 
-const createGrid = () => {
-    
+const handleSliderChange = () => {
+    emptyGrid();
+    createGrid();
 }
- 
+
+slider.addEventListener('change', handleSliderChange);
